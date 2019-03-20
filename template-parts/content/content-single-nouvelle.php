32bc6461e39296852d84747d9ac63e35889edfc6
production-jeu <43345835+production-jeu@users.noreply.github.com>
2019-03-20 12:19:26 -0400

    affichage cours par domaine

--- template-parts/content/content-single-nouvelle.php
+++ template-parts/content/content-single-nouvelle.php
@@ -19,7 +19,7 @@
 	<?php endif; ?>
 
 	<div class="entry-content">
-		<div class="wp-block-media-text alignwide" style="grid-template-columns:auto">
+		<div class="wp-block-media-text alignwide" style="grid-template-columns:auto; background-color: wheat;">
 			<figure class="wp-block-media-text__media">
 			<?php
 				$image = get_field('mon_image');
