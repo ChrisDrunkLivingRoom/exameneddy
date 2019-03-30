97a79385889b858620dfba177519a46959fdd938
production-jeu <43345835+production-jeu@users.noreply.github.com>
2019-03-30 12:28:08 -0400

    menu css et php

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -16,7 +16,7 @@
 	<header class="entry-header">
 		<nav class="menu">
 		<?php the_title( sprintf( '
-				<h2 class="entry-title" style="background-color:pink;">
+				<h2 class="entry-title">
 					<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>
 				</h2>
 			'); ?>
