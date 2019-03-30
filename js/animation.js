22c3e6ea5900226d6f12debaae14e6c255347287
production-jeu <43345835+production-jeu@users.noreply.github.com>
2019-03-30 15:02:14 -0400

    animation des boutons

--- js/animation.js
+++ js/animation.js
@@ -3,7 +3,9 @@ window.addEventListener('load',function(){
     console.log('passe')
     let elmTitre = document.querySelector('.site-branding .site-title')
     let elmDescription = document.querySelector('.site-branding .site-description')
-
+    let elmNav = document.querySelector(".site-branding .main-navigation")
+    
     elmTitre.classList.add('anim1')
+    elmNav.classList.add('anim4')
     elmDescription.classList.add('anim3')
 })
