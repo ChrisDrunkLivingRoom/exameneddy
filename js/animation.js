d38594a323473a0a0360c7b782b3a96213bc39a0
production-jeu <43345835+production-jeu@users.noreply.github.com>
2019-03-30 15:17:41 -0400

    animation selon position modulo

--- js/animation.js
+++ js/animation.js
@@ -4,8 +4,17 @@ window.addEventListener('load',function(){
     let elmTitre = document.querySelector('.site-branding .site-title')
     let elmDescription = document.querySelector('.site-branding .site-description')
     let elmNav = document.querySelector(".site-branding .main-navigation")
+    let elmCours = document.querySelectorAll(".entry-title")
     
     elmTitre.classList.add('anim1')
     elmNav.classList.add('anim4')
     elmDescription.classList.add('anim3')
+    for (let i = 0; i<elmCours.length; i++){
+        if (i%2 == 0){
+            elmCours[i].classList.add('anim3');
+        }
+        else if (i%2 != 0){
+            elmCours[i].classList.add('anim4');
+        }
+    }
 })
